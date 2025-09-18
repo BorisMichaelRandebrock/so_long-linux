6839687dc04f36e571f683866b82fdc9dc7f5385
Boris Randebrock <brandebr@student.42barcelona.com>
2025-09-18 11:30:58 +0200

    bonus & bis on the way

--- src/bonus.c
+++ src/bonus.c
@@ -28,51 +28,6 @@ void	animate_enemies(t_map *game)
 		i++;
 	}
 }
-/*
-void	move_enemies(t_map *game)
-{
-	int	i;
-	int	next_x;
-	int	next_y;
-	int	r;
-
-	i = -1;
-	while (++i < game->enemy_count)
-	{
-		// Calculate next position
-		next_x = game->enemies[i].x;
-		next_y = game->enemies[i].y;
-		if (game->enemies[i].direction == 'R')
-			next_y++;
-		else if (game->enemies[i].direction == 'L')
-			next_y--;
-		else if (game->enemies[i].direction == 'D')
-			next_x++;
-		else if (game->enemies[i].direction == 'U')
-			next_x--;
-		// Check boundaries and walls
-		if (next_x < 0 || next_x >= (int)game->height || next_y < 0
-			|| next_y >= (int)game->width || game->map[next_x][next_y] == '1')
-		{
-			r = rand() % 4;
-			if (r == 0)
-				game->enemies[i].direction = 'R';
-			else if (r == 1)
-				game->enemies[i].direction = 'L';
-			else if (r == 2)
-				game->enemies[i].direction = 'D';
-			else
-				game->enemies[i].direction = 'U';
-		}
-		else
-		{
-			game->enemies[i].x = next_x;
-			game->enemies[i].y = next_y;
-		}
-	}
-}
-
- */
 
 void	move_enemies(t_map *game)
 {
@@ -158,53 +113,6 @@ void	draw_enemies(t_map *game)
 		i++;
 	}
 }
-/*
-void	parse_enemies(t_map *game)
-{
-	int	count;
-
-	count = 0, i;
-	while (++i < (int)game->height)
-	{
-		j = -1;
-		while (++j < (int)game->width)
-			if (game->map[i][j] == 'B')
-				count++;
-	}
-	if (count > 0)
-	{
-		game->enemies = malloc(sizeof(t_enemy) * count);
-		if (!game->enemies)
-			exit_error(game, "Error\nFailed to allocate enemies\n", 0);
-		game->enemy_count = count;
-		i = -1;
-		while (++i < (int)game->height)
-		{
-			j = -1;
-			while (++j < (int)game->width)
-			{
-				if (game->map[i][j] == 'B')
-				{
-					game->enemies[idx].x = i;
-					game->enemies[idx].y = j;
-					game->enemies[idx].chaos = rand() % 10 + 1;
-					game->enemies[idx].frame = 0;
-					game->enemies[idx].step_count = 0;
-					game->enemies[idx].direction = 'R';
-					game->map[i][j] = '0';
-					idx++;
-				}
-			}
-		}
-	}
-	else
-	{
-		game->enemies = NULL;
-		game->enemy_count = 0;
-	}
-}
-
- */
 
 void	parse_enemies(t_map *game)
 {
@@ -255,7 +163,7 @@ void	parse_enemies(t_map *game)
 		game->enemy_count = 0;
 	}
 }
-
+/*
 void	ft_upload_enemies(t_map *game, int height, int width)
 {
 	int		frame;
@@ -274,46 +182,4 @@ void	ft_upload_enemies(t_map *game, int height, int width)
 			exit_error(game, "Error\nFailed to load enemy animation frame\n",
 				0);
 	}
-}
-
-/* void	parse_enemies(t_map *game)
-{
-	int	count;
-
-count = 0, i;
-// count = 0, i = -1, j, idx;
-count = 0, i = -1, j = 0, idx = 0;
-while (++i < (int)game->height)
-{
-j = -1;
-while (++j < (int)game->width)
-	if (game->map[i][j] == 'B')
-		count++;
-}
-if (count > 0)
-{
-if (!(game->enemies = malloc(sizeof(t_enemy) * count)))
-	exit_error(game, "Error\nFailed to allocate enemies\n", 0);
-game->enemy_count = count;
-i = -1;
-while (++i < (int)game->height && count > 0)
-{
-	j = -1;
-	while (++j < (int)game->width)
-	{
-		if (game->map[i][j] == 'B')
-		{
-			game->enemies[idx].x = i;
-			game->enemies[idx].y = j;
-			game->enemies[idx].chaos = rand() % 10 + 1;
-			game->enemies[idx].frame = 0;
-			game->enemies[idx].step_count = 0;
-			game->enemies[idx].direction = 'R';
-			game->map[i][j] = '0';
-			idx++;
-		}
-	}
-}
-}
-// if (count == 0) { game->enemies = NULL; game->enemy_count = 0; }
 } */
--- src/bonus_bis.c
+++ src/bonus_bis.c
@@ -19,3 +19,23 @@ void	put_movements(t_map *game)
 	snprintf(buffer, sizeof(buffer), "Movements: %d", game->count);
 	mlx_string_put(game->mlx_ptr, game->win_ptr, 15, 15, 0xFFFFFF, buffer);
 }
+
+void	ft_upload_enemies(t_map *game, int height, int width)
+{
+	int		frame;
+	char	*frame_paths[5];
+
+	frame_paths[0] = "imgs/Enemy/Anim/Bat_0.xpm";
+	frame_paths[1] = "imgs/Enemy/Anim/Bat_1.xpm";
+	frame_paths[2] = "imgs/Enemy/Anim/Bat_2.xpm";
+	frame_paths[3] = "imgs/Enemy/Anim/Bat_3.xpm";
+	frame_paths[4] = "imgs/Enemy/Anim/Bat_4.xpm";
+	for (frame = 0; frame < 5; frame++)
+	{
+		game->enemy_img[0][frame] = mlx_xpm_file_to_image(game->mlx_ptr,
+				frame_paths[frame], &width, &height);
+		if (!game->enemy_img[0][frame])
+			exit_error(game, "Error\nFailed to load enemy animation frame\n",
+				0);
+	}
+}
